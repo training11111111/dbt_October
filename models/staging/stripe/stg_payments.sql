ac97871fb8b6a092780cad624032139a3d1bdd03
flhoxha33@gmail.com <flhoxha33@gmail.com>
2023-10-13 16:51:24 +0000

    Final_commit

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,27 @@
+with 
+
+source as (
+
+    select * from {{ source('stripe', 'payment') }}
+
+),
+
+payment as (
+
+    select
+        id as payment_id,
+        orderid  as order_id,
+        paymentmethod as payment_method, 
+        status,
+        amount / 100 as amount,
+        created as created_at
+        
+
+    from source
+
+)
+
+select * from payment
+
+
+
